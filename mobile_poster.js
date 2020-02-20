bf2401d9511e4d7e6619f124076d9ce12dc5b422
SejinOH <39869184+SejinO@users.noreply.github.com>
2020-02-20 19:31:58 +0900

    change

--- mobile_poster.js
+++ mobile_poster.js
@@ -1,20 +1,16 @@
 let poster;
 
-function preload(){
-  poster = loadImage('DirtyHit_poster.png');
-}
-
 function setup() {
- createCanvas(210*2,297*2);
-colorMode(HSB);
+	createCanvas(windowWeight, windowHeight);
+	colorMode(HSB);
 }
 let x = 0;
 
 function draw() {
-background(x,255,255);
-image(poster,0,0,210*2,297*2);
+	background(x, 255, 255);
 
 }
-function deviceShaken{
-  x = random(300);
+
+function deviceShaken {
+	x = random(300);
 }
