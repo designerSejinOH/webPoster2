21c9e0c08f1a88f6f16014cbeef81a85435e3e6d
SejinOH <39869184+SejinO@users.noreply.github.com>
2020-02-20 18:44:25 +0900

    Update mobile_poster.js

--- mobile_poster.js
+++ mobile_poster.js
@@ -5,14 +5,14 @@ function preload(){
 }
 
 function setup() {
- createCanvas(210*3,297*3);
+ createCanvas(210*2,297*2);
 colorMode(HSB);
 }
 let x = 0;
 
 function draw() {
 background(x,255,255);
-image(poster,0,0,210*3,297*3);
+image(poster,0,0,210*2,297*2);
 
 }
 function deviceShaken{
